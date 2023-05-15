b9af0ca96c67607dadb46b0f1e2029104a4279d9
Valeriia Melnikov <valery.medvedeva@gmail.com>
2023-05-15 22:26:59 +0300

    feat: add recursion for opening cells

--- minesweeper/src/scripts/index.js
+++ minesweeper/src/scripts/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-use-before-define */
+/* eslint-disable no-param-reassign */
 const body = document.querySelector('.body');
 let isOver = '';
 const sizeBtn = 24;
@@ -60,6 +62,11 @@ function init(sizeX, sizeY, size) {
     cell.id = i;
     cell.classList.add(randomArr[i]);
     field.insertAdjacentElement('beforeend', cell);
+
+    cell.addEventListener('click', () => {
+      // eslint-disable-next-line no-use-before-define
+      clickOpen(cell);
+    });
   }
 }
 
@@ -95,42 +102,106 @@ function showGameModal(flag) {
     document.querySelector('.overlay').classList.remove('hide');
     document.querySelector('.modal').classList.remove('hide');
     document.querySelector('.modal').innerHTML = '\nYou lose! Try again!\n';
-    // eslint-disable-next-line quotes
-    document.querySelector('.modal').insertAdjacentHTML('afterbegin', `<img src='assets/game-icons/boom.png' alt='Boom'/>`);
+    document.querySelector('.modal')
+      // eslint-disable-next-line quotes
+      .insertAdjacentHTML('afterbegin', `<img src='assets/game-icons/boom.png' alt='Boom'/>`);
   }
   if (flag === 'win') {
     document.querySelector('.modal').innerHTML = '\nYou win! Well done!\n';
-    // eslint-disable-next-line quotes
-    document.querySelector('.modal').insertAdjacentHTML('afterbegin', `<img src='assets/game-icons/winner.png' alt='winner'/>`);
+
+    document.querySelector('.modal')
+      // eslint-disable-next-line quotes
+      .insertAdjacentHTML('afterbegin', `<img src='assets/game-icons/winner.png' alt='winner'/>`);
   }
 }
 
 function closeModal(e) {
-  const {
-    target
-  } = e;
+  const { target } = e;
   if (target.classList.contains('overlay')) {
     document.querySelector('.overlay').classList.add('hide');
     document.querySelector('.modal').classList.add('hide');
   }
 }
 
-function clickOpen(e) {
-  const { target } = e;
-  const currentCell = target.id;
-  console.log(target);
-  if (target.classList.contains('cell')) {
-    if (target.classList.contains('open')) return;
-    if (target.classList.contains('boom')) {
+function clickOpen(cell) {
+  console.log(cell);
+  const currentNumber = +cell.getAttribute('number');
+  const currentId = +cell.getAttribute('id');
+  console.log(cell, currentId);
+  if (cell.classList.contains('cell')) {
+    if (cell.classList.contains('open')) return;
+    if (cell.classList.contains('boom')) {
       isOver = 'loose';
       showGameModal(isOver);
-    } else if (+target.getAttribute('number') === 0) {
-      console.log('!');
+    } else if (currentNumber === 0) {
+      checkArea(currentId);
+      cell.classList.add('open');
+      if (currentNumber === 0) cell.style.backgroundColor = '#a6f1a6';
+      cell.style.border = 'none';
+    } else {
+      cell.textContent = currentNumber;
+      cell.classList.add('open');
+      cell.style.border = 'none';
+      if (currentNumber === 1) cell.style.backgroundColor = '#90ee90';
+      if (currentNumber === 2) cell.style.backgroundColor = '#C4E97D';
+      if (currentNumber === 3) cell.style.backgroundColor = '#FFDA84';
+      if (currentNumber === 4) cell.style.backgroundColor = '#FFC75F';
+      if (currentNumber === 5) cell.style.backgroundColor = '#FF9671';
+      if (currentNumber === 6) cell.style.backgroundColor = '#FF847D';
+      if (currentNumber === 7) cell.style.backgroundColor = '#FF6F91';
+      if (currentNumber === 8) cell.style.backgroundColor = '#D65DB1';
+      // eslint-disable-next-line no-useless-return
+      return;
     }
-    target.classList.add('open');
   }
 }
-
-document.addEventListener('click', clickOpen);
+function checkArea(id) {
+  const leftSide = id % widthField === 0;
+  const rigthSide = id % widthField === widthField - 1;
+  const topSide = id / heightField < 1;
+  const bottomSide = id / heightField >= heightField - 1;
+  setTimeout(() => {
+    if (!topSide) {
+      const newId = id - widthField;
+      const newCell = document.getElementById(newId);
+      clickOpen(newCell);
+    }
+    if (!bottomSide) {
+      const newId = id + widthField;
+      const newCell = document.getElementById(newId);
+      clickOpen(newCell);
+    }
+    if (!leftSide) {
+      const newId = id - 1;
+      const newCell = document.getElementById(newId);
+      clickOpen(newCell);
+    }
+    if (!rigthSide) {
+      const newId = id + 1;
+      const newCell = document.getElementById(newId);
+      clickOpen(newCell);
+    }
+    if (!topSide && !leftSide) {
+      const newId = id - widthField - 1;
+      const newCell = document.getElementById(newId);
+      clickOpen(newCell);
+    }
+    if (!topSide && !rigthSide) {
+      const newId = id - widthField + 1;
+      const newCell = document.getElementById(newId);
+      clickOpen(newCell);
+    }
+    if (!bottomSide && !leftSide) {
+      const newId = id + widthField - 1;
+      const newCell = document.getElementById(newId);
+      clickOpen(newCell);
+    }
+    if (!bottomSide && !rigthSide) {
+      const newId = id + widthField + 1;
+      const newCell = document.getElementById(newId);
+      clickOpen(newCell);
+    }
+  }, 20);
+}
 
 document.addEventListener('click', closeModal);
