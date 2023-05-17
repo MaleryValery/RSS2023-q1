4d3799eac7630d3375dae99344c12be631abab35
Valeriia Melnikov <valery.medvedeva@gmail.com>
2023-05-17 17:42:49 +0300

    feat: first move is not fail,condition for win

--- minesweeper/src/scripts/index.js
+++ minesweeper/src/scripts/index.js
@@ -1,24 +1,16 @@
+/* eslint-disable no-unused-expressions */
 /* eslint-disable no-use-before-define */
 /* eslint-disable no-param-reassign */
 /* eslint-disable no-useless-return */
 import generateNumbers from './generateNumbers.js';
-import showGameModal from './showGameModal.js';
+import { showGameModal, checkIfwin } from './showGameModal.js';
 
 const body = document.querySelector('.body');
 let isOver = '';
 const sizeCell = 24;
-const widthField = 10;
-const heightField = 10;
+const widthField = 5;
+const heightField = 5;
 const arrLenght = widthField * heightField;
-const qtyBoom = 10;
-const qtyFlag = qtyBoom;
-let move = 0;
-
-const lenghtEmptyArr = arrLenght - qtyBoom;
-const boomArr = new Array(qtyBoom).fill('boom');
-const cellsArr = new Array(lenghtEmptyArr).fill('empty');
-const mainArr = boomArr.concat(cellsArr);
-let randomArr;
 
 const modal = document.createElement('div');
 modal.className = 'modal hide';
@@ -80,48 +72,75 @@ levelBomb.className = 'level-bomb';
 levelBomb.placeholder = '10';
 levelBox.insertAdjacentElement('beforeend', levelBomb);
 
+const qtyBoom = +levelBomb.textContent || 10;
+const qtyFlag = qtyBoom;
+let move = 0;
+
+const lenghtEmptyArr = arrLenght - qtyBoom;
+const boomArr = new Array(qtyBoom).fill('boom');
+const emptysArr = new Array(lenghtEmptyArr).fill('empty');
+const mainArr = boomArr.concat(emptysArr);
+let randomArr;
+let chengetArr;
+console.log(levelBomb.value);
+
 function init(sizeX, sizeY) {
   move = 0;
+  const tempArr = [];
   randomArr = mainArr.sort(() => Math.random() - 0.5);
+  for (let i = 0; i < randomArr.length; i += 1) {
+    if (randomArr[i] === 'empty') tempArr.push(i);
+  }
+  console.log(randomArr);
   for (let i = 0; i < sizeX * sizeY; i += 1) {
     const cell = document.createElement('div');
     cell.className = 'cell';
     cell.id = i;
     field.insertAdjacentElement('beforeend', cell);
 
+    // eslint-disable-next-line no-loop-func
     cell.addEventListener('click', () => {
       move += 1;
       console.log(move);
-      if (move === 1) {
-        console.log('first step', cell.id);
+      if (move === 1 && randomArr[cell.id] === 'boom') {
+        const firstBoom = cell.id;
+        const index = Math.floor(Math.random() * tempArr.length);
+        randomArr[tempArr[index]] = 'boom';
+        randomArr[firstBoom] = 'empty';
+        chengetArr = [...randomArr];
+        generateNumbers(widthField, heightField, chengetArr);
       }
-      clickOpen(cell, randomArr);
+      clickOpen(cell, chengetArr);
     });
 
     cell.addEventListener('contextmenu', (e) => {
       e.preventDefault();
-      pickFlag(cell);
+      pickFlag(cell, qtyFlag);
     });
   }
 }
 init(widthField, heightField);
+
 generateNumbers(widthField, heightField, randomArr);
 
-function pickFlag(cell) {
+function pickFlag(cell, flags) {
   if (!cell.classList.contains('open') && !cell.classList.contains('flag')) {
     cell.classList.add('flag');
     cell.insertAdjacentHTML('afterbegin', '<img src = \'assets/game-icons/red-flag.png\' width=16px height =18px>');
+    flags -= 1;
   } else if (!cell.classList.contains('open') && cell.classList.contains('flag')) {
     cell.classList.remove('flag');
     cell.innerHTML = '';
+    flags += 1;
   }
-  if (isOver === 'lose') return;
+  if (isOver === 'lose' || isOver === 'win') return;
 }
 
 function clickOpen(cell, arrCells = randomArr) {
   const currentNumber = +cell.getAttribute('number');
   const currentId = +cell.getAttribute('id');
-  if (cell.classList.contains('open') || cell.classList.contains('flag')) return;
+  const cells = document.querySelectorAll('.cell');
+  if (cell.classList.contains('open') || cell.classList.contains('flag') || cell.classList.contains('boom')) return;
   if (arrCells[currentId] === 'boom') {
     isOver = 'loose';
     showGameModal(isOver, arrCells);
@@ -130,6 +149,7 @@ function clickOpen(cell, arrCells = randomArr) {
     cell.classList.add('open');
     if (currentNumber === 0) cell.style.backgroundColor = '#a6f1a6';
     cell.style.border = 'none';
+    checkIfwin(arrCells, cells, emptysArr, qtyBoom, isOver);
   } else {
     cell.textContent = currentNumber;
     cell.classList.add('open');
@@ -142,6 +162,7 @@ function clickOpen(cell, arrCells = randomArr) {
     if (currentNumber === 6) cell.style.backgroundColor = '#FF847D';
     if (currentNumber === 7) cell.style.backgroundColor = '#FF6F91';
     if (currentNumber === 8) cell.style.backgroundColor = '#D65DB1';
+    checkIfwin(arrCells, cells, emptysArr, qtyBoom, isOver);
     // eslint-disable-next-line no-useless-return
     return;
   }
@@ -154,43 +175,35 @@ function checkArea(id) {
   const bottomSide = id / heightField >= heightField - 1;
   setTimeout(() => {
     if (!leftSide) {
-      const newId = id - 1;
-      const newCell = document.getElementById(newId);
+      const newCell = document.getElementById(id - 1);
       clickOpen(newCell);
     }
     if (!rigthSide) {
-      const newId = id + 1;
-      const newCell = document.getElementById(newId);
+      const newCell = document.getElementById(id + 1);
       clickOpen(newCell);
     }
     if (!topSide) {
-      const newId = id - widthField;
-      const newCell = document.getElementById(newId);
+      const newCell = document.getElementById(id - widthField);
       clickOpen(newCell);
     }
     if (!bottomSide) {
-      const newId = id + widthField;
-      const newCell = document.getElementById(newId);
+      const newCell = document.getElementById(id + widthField);
       clickOpen(newCell);
     }
     if (!topSide && !leftSide) {
-      const newId = id - widthField - 1;
-      const newCell = document.getElementById(newId);
+      const newCell = document.getElementById(id - widthField - 1);
       clickOpen(newCell);
     }
     if (!topSide && !rigthSide) {
-      const newId = id - widthField + 1;
-      const newCell = document.getElementById(newId);
+      const newCell = document.getElementById(id - widthField + 1);
       clickOpen(newCell);
     }
     if (!bottomSide && !leftSide) {
-      const newId = id + widthField - 1;
-      const newCell = document.getElementById(newId);
+      const newCell = document.getElementById(id + widthField - 1);
       clickOpen(newCell);
     }
     if (!bottomSide && !rigthSide) {
-      const newId = id + widthField + 1;
-      const newCell = document.getElementById(newId);
+      const newCell = document.getElementById(id + widthField + 1);
       clickOpen(newCell);
     }
   }, 20);
@@ -205,3 +218,18 @@ function closeModal(e) {
 }
 
 document.addEventListener('click', closeModal);
+
+// function checkIfwin(arr, cells, emptysArr, gameStatus) {
+//   let cellOpen = 0;
+//   cells.forEach((cell, i) => {
+//     if (cell.classList.contains('open') && arr[i] !== 'boom') {
+//       cellOpen += 1;
+//       console.log(cellOpen, emptysArr.length);
+//     }
+//     if (cellOpen === emptysArr.length) {
+//       gameStatus = 'win';
+//       console.log(gameStatus, gameStatus === 'win');
+//       showGameModal(gameStatus, arr);
+//     }
+//   });
+// }
--- minesweeper/src/scripts/showGameModal.js
+++ minesweeper/src/scripts/showGameModal.js
@@ -5,7 +5,10 @@ function showGameModal(gameStatus, arr) {
     cells.forEach((el, i) => {
       const qtyBomb = +el.getAttribute('number');
       if (arr[i] === 'boom') {
-        el.insertAdjacentHTML('afterbegin', '<img src = \'assets/game-icons/bomb.png\' width=22px height =22px>');
+        el.classList.add('boom');
+        if (el.classList.contains('boom') && !el.classList.contains('flag')) {
+          el.insertAdjacentHTML('afterbegin', '<img src = \'assets/game-icons/bomb.png\' width=22px height =22px>');
+        }
       } else {
         el.classList.add('open');
         el.style.border = 'none';
@@ -28,12 +31,51 @@ function showGameModal(gameStatus, arr) {
       .insertAdjacentHTML('afterbegin', `<img src='assets/game-icons/boom.png' alt='Boom'/>`);
   }
   if (gameStatus === 'win') {
+    cells.forEach((el, i) => {
+      const qtyBomb = +el.getAttribute('number');
+      if (arr[i] === 'boom') {
+        el.classList.add('boom');
+        if (el.classList.contains('boom') && !el.classList.contains('flag')) {
+          el.innerHTML = '';
+          el.insertAdjacentHTML('afterbegin', '<img src = \'assets/game-icons/red-flag.png\' width=18px height =18px>');
+        }
+      } else {
+        el.classList.add('open');
+        el.style.border = 'none';
+        el.innerHTML = qtyBomb === 0 ? '' : qtyBomb;
+        if (qtyBomb === 1) el.style.backgroundColor = '#90ee90';
+        if (qtyBomb === 2) el.style.backgroundColor = '#C4E97D';
+        if (qtyBomb === 3) el.style.backgroundColor = '#FFDA84';
+        if (qtyBomb === 4) el.style.backgroundColor = '#FFC75F';
+        if (qtyBomb === 5) el.style.backgroundColor = '#FF9671';
+        if (qtyBomb === 6) el.style.backgroundColor = '#FF847D';
+        if (qtyBomb === 7) el.style.backgroundColor = '#FF6F91';
+        if (qtyBomb === 8) el.style.backgroundColor = '#D65DB1';
+      }
+    });
+    document.querySelector('.overlay').classList.remove('hide');
+    document.querySelector('.modal').classList.remove('hide');
     document.querySelector('.modal').innerHTML = '\nYou win! Well done!\n';
-
     document.querySelector('.modal')
-      // eslint-disable-next-line quotes
-      .insertAdjacentHTML('afterbegin', `<img src='assets/game-icons/winner.png' alt='winner'/>`);
+      .insertAdjacentHTML('afterbegin', '<img src=\'assets/game-icons/winner.png\' alt=\'winner\'/>');
   }
 }
 
-export default showGameModal;
+function checkIfwin(arr, cells, emptysArr, booms, gameStatus) {
+  let cellOpen = 0;
+  let flagAndBoom = 0;
+  cells.forEach((cell, i) => {
+    if ((cell.classList.contains('open') && arr[i] !== 'boom')) {
+      cellOpen += 1;
+    }
+    if (cell.classList.contains('flag') && arr[i] === 'boom') {
+      flagAndBoom += 1;
+    }
+    if (cellOpen === emptysArr.length) {
+      gameStatus = 'win';
+      showGameModal(gameStatus, arr);
+    }
+  });
+}
+
+export { showGameModal, checkIfwin };
