d02862b1337c0628f27e83d898064086b0463f5a
Valeriia Melnikov <valery.medvedeva@gmail.com>
2023-06-11 00:19:38 +0300

    fix: correct sppView.ts file

--- migration-newip-to-ts/src/components/view/appView.ts
+++ migration-newip-to-ts/src/components/view/appView.ts
@@ -1,20 +1,20 @@
 import News from './news/news';
 import Sources from './sources/sources';
-import { NewsDrawInterface, SourcesDrawInterface } from '../app/interface';
+import { NewsDrawInterface, SourcesDrawInterface, AppViewInterface } from '../app/interface';
 import { RespNewsType, RespSourceType } from '../app/types';
 
-export class AppView {
-  constructor(private news: NewsDrawInterface, private sources: SourcesDrawInterface) {
+export class AppView implements AppViewInterface {
+  constructor(public news: NewsDrawInterface, public sources: SourcesDrawInterface) {
     this.news = new News();
     this.sources = new Sources();
   }
 
-  private drawNews(data: RespNewsType): void {
+  public drawNews(data: RespNewsType): void {
     const values = data?.articles ? data?.articles : [];
     this.news.draw(values);
   }
 
-  private drawSources(data: RespSourceType): void {
+  public drawSources(data: RespSourceType): void {
     const values = data?.sources ? data?.sources : [];
     this.sources.draw(values);
   }
