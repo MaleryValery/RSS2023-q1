291c9a875d4c1d46a72ef0d4386df2c97cbee722
Valeriia Melnikov <valery.medvedeva@gmail.com>
2023-06-12 15:47:48 +0300

    fix: change link for deploy

--- migration-newip-to-ts/src/components/controller/appLoader.ts
+++ migration-newip-to-ts/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
   constructor() {
-    super('https://newsapi.org/v2/', {
+    super('https://rss-news-api.onrender.com/', {
       apiKey: '35c9f99c8367463e87a3c21225c94af8', // получите свой ключ https://newsapi.org/
     });
   }
